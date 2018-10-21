14cd7607d6697310117911292169b59cf238704d
ArunaMaurya221B <aruna.maurya12@gmail.com>
2018-10-21 23:16:07 +0530

    Added code for testing. The output of prediction returns the corresponding folder number in the training set. Implementation for a single image .

--- cnn.py
+++ cnn.py
@@ -2,7 +2,7 @@ import cv2
 import tensorflow as tf
 from tensorflow import keras
 import pandas as pd
-from keras.datasets import mnist
+import numpy as np
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -55,6 +55,9 @@ cnn_model = keras.Sequential([
 	])
 
 cnn_model.summary()
+
+
+
 cnn_model.compile(optimizer=tf.train.AdamOptimizer(),
 	loss='categorical_crossentropy',
 	metrics=['accuracy'])
@@ -63,10 +66,14 @@ cnn_model.compile(optimizer=tf.train.AdamOptimizer(),
 cnn_model.fit_generator(
 	train_generator,
 	steps_per_epoch=290,#	number of training example photos
-	epochs=6,
+	epochs=3,
 	validation_data=validation_generator,
 	validation_steps=14 #number of testing photos
 	)
 
-predictions = cnn_model.predict()
-print predictions[1]
+from keras.preprocessing import image
+test_image = image.load_img('/home/icts/practice-datasets/OCR/data/validation/img062-00004.png', target_size = (150,150))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = cnn_model.predict(test_image)
+print np.argmax(result)
